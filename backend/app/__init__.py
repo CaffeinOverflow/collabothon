ed9f38e0f2009051c3dc429a23c3513c993c9334
Robert Crupa <robert.crupa@clearcom.com>
2023-10-20 15:40:25 +0100

    Modify app to work with firebase instead of sqlLite

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
 from flask_moment import Moment
-from backend.config import config
+from config import config
+from firebase_admin import credentials, firestore, initialize_app
+from google.auth import default
+
+# Initialize Firestore DB
+
+
+
+db = firestore.Client(project="collabothon23fra-1265")
+todo_ref = db.collection('todos')
 
 bootstrap = Bootstrap()
-db = SQLAlchemy()
 
 
 def create_app(config_name):
@@ -14,7 +21,6 @@ def create_app(config_name):
     config[config_name].init_app(app)
 
     bootstrap.init_app(app)
-    db.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
