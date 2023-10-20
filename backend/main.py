2b9e1c7d4c725ef034e2ae527266ea6e842eed6a
Robert Crupa <Robert.Crupa@Clearcom.com>
2023-10-20 16:50:24 +0100

    Add firebase db (#5)
    * moved flask app into backend folder
    
    * Modify app to work with firebase instead of sqlLite
    
    * Added docker and cloud build file to run app in google run, not working

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ import click
 from flask_migrate import Migrate
 from app import create_app, db
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+app =  create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 
 
