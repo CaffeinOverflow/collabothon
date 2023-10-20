09e319c24ed12b9853286d3dcbb58999396f7526
Dan Goje <gojedan98@gmail.com>
2023-10-20 18:42:01 +0300

    Add Map Component (#6)
    * Add initial frontend w/ leaderboard
    
    * Add Map component
    
    * linting

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { LoadScript } from '@react-google-maps/api';
 import App from "./App";
-import Leaderboard from "./components/Leaderboard"
+import Leaderboard from "./components/Leaderboard/Leaderboard";
+import Map from "./components/Map/Map";
 
 const rootDiv = document.getElementById('root');
 if (!rootDiv) {
@@ -10,13 +12,34 @@ if (!rootDiv) {
 }
 const root = ReactDOM.createRoot(rootDiv);
 
+const BINS = [
+  {
+    id: "bin1",
+    lat: 50.103323,
+    lng: 14.392298,
+    color: "red",
+  },
+  {
+    id: "bin2",
+    lat: 50.123323,
+    lng: 14.412298,
+    color: "blue",
+  },
+]
+const MAPS_API_KEY="<add your key here>"
+
 function Root() {
+  const [showMap, setShowMap] = useState(false);
   return (
-    <React.StrictMode>
+    <LoadScript googleMapsApiKey={MAPS_API_KEY}>
       <App>
-        <Leaderboard />
+        {
+          showMap ?
+          <Map  bins={BINS} backToLeaderboard={() => setShowMap(false) } /> :
+          <Leaderboard toMap={() => setShowMap(true)} />
+        }
       </App>
-    </React.StrictMode>
+    </LoadScript>
   );
 }
 root.render(<Root />);
