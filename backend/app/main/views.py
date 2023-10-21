8b2c6409e717d5a8df28acf2dc3dd2fc951f6274
Robert Crupa <robert.crupa@clearcom.com>
2023-10-21 10:25:40 +0100

    Removed unnecessary blueprint

--- backend/app/main/views.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, session, redirect, url_for, current_app
-from .. import db
-from . import main
-
-
-@main.route('/', methods=['GET', 'POST'])
-def index():
-    return redirect(url_for('.index'))
