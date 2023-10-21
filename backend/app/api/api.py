8f499a3fd8cfc6741b689f6a0ab5265e7c5568a4
Robert Crupa <robert.crupa@clearcom.com>
2023-10-21 10:33:49 +0100

    Defined backend API

--- backend/app/api/api.py
+++ backend/app/api/api.py
@@ -1,10 +1,35 @@
-from flask import jsonify, request, g, url_for, current_app
+from flask import jsonify, request
 from . import api
+from models import CollectionEvent, Bin, BinStats
+from app import db
 
 @api.post('/bin/<int:id>/collection/')
-def add_allocation():
-    pass
+def create_collection_event(binId):
+    try:
+        data = request.get_json()
+        bin = db.child("bins").child(binId).get().val()
+
+        if not bin:
+            return jsonify({"error": "Bin not found"}), 404
+
+        category = data.get("category")
+        weight = data.get("weight")
+        
+        if category not in ["General", "Plastic"]:
+            return jsonify({"error": "Invalid category"}), 400
+
+        collection_event = CollectionEvent(binId, category, weight)
+        db.child("collectionEvents").push(collection_event.__dict__)
+        
+        return jsonify({"collectionId": collection_event.id}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 @api.get('/bin/stat/')
 def get_bin_stats():
-    pass
+    try:
+        stats = db.child("binStats").get().val()
+        return jsonify({"stats": stats}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
