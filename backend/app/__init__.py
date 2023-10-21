7dc47975dd14ac54ef3fe57ad83ea8b5b477ec2d
Robert Crupa <robert.crupa@clearcom.com>
2023-10-21 10:46:08 +0100

    Registering api blueprint

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -22,7 +22,7 @@ def create_app(config_name):
 
     bootstrap.init_app(app)
 
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+    from .api import api as api_blueprint
+    app.register_blueprint(api_blueprint)
 
     return app
